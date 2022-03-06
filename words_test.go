e765c7d253c5378e2dc07b357b880ccf81490d06
James Gale <jlgale@gmail.com>
2022-03-06 10:00:56 -0800

    more words testcase

--- words_test.go
+++ words_test.go
@@ -1,11 +1,28 @@
 package wordle
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func ParseWordTest(t *testing.T) {
+func mkw(s string) Word {
+	w, err := ParseWord(s)
+	if err != nil {
+		panic(err)
+	}
+	return w
+}
+
+func mkm(s string) Match {
+	m, err := ParseMatch(s)
+	if err != nil {
+		panic(err)
+	}
+	return m
+}
+
+func TestParseWord(t *testing.T) {
 	_, err := ParseWord("yuk")
 	assert.Error(t, err)
 	_, err = ParseWord("ab de")
@@ -15,4 +32,14 @@ func ParseWordTest(t *testing.T) {
 	w2, err := ParseWord("HMBLE")
 	assert.NoError(t, err, "parse failed")
 	assert.Equal(t, w1, w2)
+}
+
+func TestWordMatch(t *testing.T) {
+	a := mkw("cloth")
+	assert.Equal(t, mkm("..y.."), mkw("petar").Match(a))
+	assert.Equal(t, mkm("....y"), mkw("quint").Match(a))
+	assert.Equal(t, mkm(".ygy."), mkw("stock").Match(a))
+	assert.Equal(t, mkm("ggggg"), mkw("cloth").Match(a))
+
+	assert.Equal(t, mkm("..y.."), mkw("fuzzy").Match(mkw("zilch")))
 }
