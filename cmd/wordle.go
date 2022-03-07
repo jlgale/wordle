a9fbf7e7eff12925d47a997c445209536771e3a2
James Gale <jlgale@gmail.com>
2022-03-06 17:25:53 -0800

    cleanup commandline options further

--- cmd/wordle.go
+++ cmd/wordle.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"reflect"
+	"runtime/pprof"
 	"sort"
 	"strings"
 	"time"
@@ -23,71 +24,63 @@ func play(game *wordle.Game, strategy wordle.Strategy, answer wordle.Word) {
 }
 
 func main() {
-	var words []wordle.Word
-	var strategy wordle.Strategy
-	var log zerolog.Logger = zerolog.New(os.Stderr).Level(zerolog.InfoLevel)
-	var rng *rand.Rand
-	var playStrategy string
-	var wordFilePath string
-	var randomSeed int64
-	var debugLogging bool
-	var pow float64
-	var scale string
-	var fallbackStrategy string
-
 	root := &cobra.Command{
 		Use:   "wordle",
 		Short: "Play wordle games.",
 		Long: (`A utility for playing "wordle" games on the commandline. ` +
 			`Useful for exploring playing strategies.`),
 	}
-	root.PersistentFlags().StringVar(&wordFilePath, "words", "./words",
+	wordsOpt := root.PersistentFlags().String("words", "./words",
 		"Path to accepted word list")
-	root.PersistentFlags().Int64Var(&randomSeed, "seed", 0, "Random seed")
-	root.PersistentFlags().StringVarP(&playStrategy, "strategy", "s", "filtering",
+	seedOpt := root.PersistentFlags().Int64("seed", 0, "Random seed")
+	strategyOpt := root.PersistentFlags().StringP("strategy", "s", "filtering",
 		"Play strategy. One of: common, diversity, filtering, naive, selective")
-	root.PersistentFlags().BoolVarP(&debugLogging, "debug", "d", false,
+	debugOpt := root.PersistentFlags().BoolP("debug", "d", false,
 		"Enable debug logging")
-	root.PersistentFlags().StringVar(&scale, "scale", "random",
-		"Choose among weighted words")
-	root.PersistentFlags().Float64Var(&pow, "pow", 1.0,
+	scaleOpt := root.PersistentFlags().String("scale", "random",
+		"Choose among weighted words. One of: random, top")
+	expOpt := root.PersistentFlags().Float64("exp", 1.0,
 		"Scale weighted strategy by this exponent")
-	root.PersistentFlags().StringVar(&fallbackStrategy, "fallback", "diversity",
+	fallbackOpt := root.PersistentFlags().String("fallback", "diversity",
 		"Fallback strategy when a simpler strategy is needed")
 
 	// Setup common state
+	var words []wordle.Word
+	var strategy wordle.Strategy
+	var log zerolog.Logger = zerolog.New(os.Stderr).Level(zerolog.InfoLevel)
+	var rng *rand.Rand
 	root.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		if debugLogging {
+		if *debugOpt {
 			log = log.Level(zerolog.DebugLevel)
 		}
 		var err error
-		words, err = readWordFile(wordFilePath, func(word string, lineno int, err error) error {
-			log.Printf("%s:%d: %s: %v\n", wordFilePath, lineno, word, err)
+		words, err = readWordFile(*wordsOpt, func(word string, lineno int, err error) error {
+			log.Printf("%s:%d: %s: %v\n", *wordsOpt, lineno, word, err)
 			return nil
 		})
 		if err != nil {
 			return err
 		}
-		log.Printf("%s: loaded %d words", wordFilePath, len(words))
-		if randomSeed == 0 {
-			randomSeed = time.Now().UnixNano()
+		log.Printf("%s: loaded %d words", *wordsOpt, len(words))
+		if *seedOpt == 0 {
+			*seedOpt = time.Now().UnixNano()
 		}
-		rng = rand.New(rand.NewSource(randomSeed))
+		rng = rand.New(rand.NewSource(*seedOpt))
 		var scalefn func(s wordle.Scale) wordle.Strategy
-		switch strings.ToLower(scale) {
+		switch strings.ToLower(*scaleOpt) {
 		case "random":
 			scalefn = func(s wordle.Scale) wordle.Strategy {
-				return wordle.WeightedStrategy(rng, s, pow)
+				return wordle.WeightedStrategy(rng, s, *expOpt)
 			}
 		case "top":
 			scalefn = func(s wordle.Scale) wordle.Strategy {
 				return wordle.TopStrategy(rng, s)
 			}
 		default:
-			return fmt.Errorf("Unrecognized scale function: %s", scale)
+			return fmt.Errorf("Unrecognized scale function: %s", *scaleOpt)
 		}
 
-		if debugLogging {
+		if *debugOpt {
 			// Wrap a logger around the scale function
 			innerScaleFn := scalefn
 			scalefn = func(s wordle.Scale) wordle.Strategy {
@@ -108,153 +101,145 @@ func main() {
 			default:
 				return nil, fmt.Errorf("Unrecognized fallback strategy: %s", name)
 			}
-			if debugLogging {
+			if *debugOpt {
 				strategy = &loggingStrategy{strategy, &log}
 			}
 			return
 		}
-		fallback, err := mkStrategy(fallbackStrategy)
+		fallback, err := mkStrategy(*fallbackOpt)
 		if err != nil {
 			return err
 		}
 
-		switch strings.ToLower(playStrategy) {
+		switch strings.ToLower(*strategyOpt) {
 		case "filtering":
 			strategy = wordle.FilteringStrategy(rng, &log, fallback)
-			if debugLogging {
+			if *debugOpt {
 				strategy = &loggingStrategy{strategy, &log}
 			}
 		default:
-			strategy, err = mkStrategy(playStrategy)
+			strategy, err = mkStrategy(*strategyOpt)
 			if err != nil {
 				return err
 			}
 		}
+
 		return nil
 	}
 
-	interact := &cobra.Command{
-		Use:   "interact",
-		Short: "Interactively guess a wordle answer.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			game := wordle.NewGame(words, nil)
-			for !game.Over() {
-				guess := strategy.Guess(&game)
-			force:
-				fmt.Println("My guess", guess)
-				var matchString string
-				for {
-					fmt.Print(`describe match (or "again" for a different guess): `)
-					fmt.Scanf("%s", &matchString)
-					// In case the chosen word is not allowed:
-					if strings.ToLower(matchString) == "again" {
-						game.RemoveWord(guess)
-						break
-					}
-					if strings.ToLower(matchString) == "force" {
-						fmt.Print(`give me a word to play: `)
-						var guessString string
-						fmt.Scanf("%s", &guessString)
-						var err error
-						guess, err = wordle.ParseWord(guessString)
-						if err != nil {
-							fmt.Println(err)
-							continue
-						}
-						goto force
-					}
-					match, err := wordle.ParseMatch(matchString)
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					game = game.Guess(guess, match)
+	interactCmd := &cobra.Command{Use: "interact", Short: "Interactively guess a wordle answer."}
+	interactCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		game := wordle.NewGame(words, nil)
+		for !game.Over() {
+			guess := strategy.Guess(&game)
+			fmt.Println("My guess", guess)
+			var matchString string
+			for {
+				fmt.Print(`describe match (or "again" for a different guess): `)
+				fmt.Scanf("%s", &matchString)
+				// In case the chosen word is not allowed:
+				if strings.ToLower(matchString) == "again" {
+					game.RemoveWord(guess)
 					break
 				}
-			}
-			switch len(game.Guesses) {
-			case 1:
-				fmt.Println("Genius")
-			case 2:
-				fmt.Println("Magnificent")
-			case 3:
-				fmt.Println("Impressive")
-			case 4:
-				fmt.Println("Splendid")
-			case 5:
-				fmt.Println("Great")
-			case 6:
-				fmt.Println("Phew")
-			}
-			return nil
-		},
-	}
-	var repeat int
-	var answerFilePath string
-	play := &cobra.Command{
-		Use:   "play",
-		Short: "Play automatically.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			answers := make([]wordle.Word, len(args))
-			for idx, s := range args {
-				answer, err := wordle.ParseWord(s)
+				match, err := wordle.ParseMatch(matchString)
 				if err != nil {
-					return fmt.Errorf("%s: %w", s, err)
+					fmt.Println(err)
+					continue
 				}
-				answers[idx] = answer
+				game = game.Guess(guess, match)
+				break
 			}
-			if answerFilePath != "" {
-				loaded, err := readWordFile(answerFilePath, func(word string, lineno int, err error) error {
-					log.Printf("%s:%d: %s: %v\n", answerFilePath, lineno, word, err)
-					return nil
-				})
+		}
+		switch len(game.Guesses) {
+		case 1:
+			fmt.Println("Genius")
+		case 2:
+			fmt.Println("Magnificent")
+		case 3:
+			fmt.Println("Impressive")
+		case 4:
+			fmt.Println("Splendid")
+		case 5:
+			fmt.Println("Great")
+		case 6:
+			fmt.Println("Phew")
+		}
+		return nil
+	}
+
+	playCmd := &cobra.Command{Use: "play", Short: "Play automatically."}
+	repeatOpt := playCmd.Flags().IntP("repeat", "n", 0, "Play multiple games.")
+	answersOpt := playCmd.Flags().StringP("answers", "a", "", "Load answers from a file.")
+	cpuProfileOpt := root.PersistentFlags().String("cpu-profile", "",
+		"Profile CPU usage and write the given file")
+	playCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		answers := make([]wordle.Word, len(args))
+		for idx, s := range args {
+			answer, err := wordle.ParseWord(s)
+			if err != nil {
+				return fmt.Errorf("%s: %w", s, err)
+			}
+			answers[idx] = answer
+		}
+		if *answersOpt != "" {
+			loaded, err := readWordFile(*answersOpt, func(word string, lineno int, err error) error {
+				log.Printf("%s:%d: %s: %v\n", *answersOpt, lineno, word, err)
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+			answers = append(answers, loaded...)
+		}
+		if *repeatOpt == 0 {
+			for _, answer := range answers {
+				game := wordle.NewGame(words, nil)
+				play(&game, strategy, answer)
+				fmt.Println(game)
+				if !game.Won() {
+					fmt.Println("The answer was:", answer)
+				}
+			}
+		} else if *repeatOpt > 0 {
+			if *cpuProfileOpt != "" {
+				f, err := os.Create(*cpuProfileOpt)
 				if err != nil {
 					return err
 				}
-				answers = append(answers, loaded...)
+				pprof.StartCPUProfile(f)
+				defer pprof.StopCPUProfile()
 			}
-			if repeat == 0 {
+
+			var guesses int
+			var minGuesses int = 7
+			var maxGuesses int = 0
+			var wins int = 0
+			for i := 0; i < *repeatOpt; i++ {
 				for _, answer := range answers {
+					log.Debug().Stringer("answer", answer).Msg("New Game")
 					game := wordle.NewGame(words, nil)
 					play(&game, strategy, answer)
-					fmt.Println(game)
-					if !game.Won() {
-						fmt.Println("The answer was:", answer)
+					if game.Won() {
+						wins += 1
+					}
+					guesses += len(game.Guesses)
+					if len(game.Guesses) > maxGuesses {
+						maxGuesses = len(game.Guesses)
+					}
+					if len(game.Guesses) < minGuesses {
+						minGuesses = len(game.Guesses)
 					}
 				}
-			} else if repeat > 0 {
-				var guesses int
-				var minGuesses int = 7
-				var maxGuesses int = 0
-				var wins int = 0
-				for i := 0; i < repeat; i++ {
-					for _, answer := range answers {
-						log.Debug().Stringer("answer", answer).Msg("New Game")
-						game := wordle.NewGame(words, nil)
-						play(&game, strategy, answer)
-						if game.Won() {
-							wins += 1
-						}
-						guesses += len(game.Guesses)
-						if len(game.Guesses) > maxGuesses {
-							maxGuesses = len(game.Guesses)
-						}
-						if len(game.Guesses) < minGuesses {
-							minGuesses = len(game.Guesses)
-						}
-					}
-				}
-				games := repeat * len(answers)
-				fmt.Printf("Won %d of %d games (%0.1f%%). Guesses: avg %0.1f, min %d, max %d\n",
-					wins, games, float64(wins)/float64(games)*100, float64(guesses)/float64(games),
-					minGuesses, maxGuesses)
 			}
-			return nil
-		},
+			games := *repeatOpt * len(answers)
+			fmt.Printf("Won %d of %d games (%0.1f%%). Guesses: avg %0.1f, min %d, max %d\n",
+				wins, games, float64(wins)/float64(games)*100, float64(guesses)/float64(games),
+				minGuesses, maxGuesses)
+		}
+		return nil
 	}
-	play.Flags().IntVarP(&repeat, "repeat", "n", 0, "Play multiple games.")
-	play.Flags().StringVarP(&answerFilePath, "answers", "a", "", "Load answers from a file.")
-	root.AddCommand(interact, play)
+	root.AddCommand(interactCmd, playCmd)
 	root.Execute()
 }
 
